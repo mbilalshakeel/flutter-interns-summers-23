10c18a522894246f6c7ddfe79a8339ce148fa7bc
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-07-20 13:21:37 +0500

    Folders and make files

---
+++ lib/assesments/assessment_2/fi_a2_muhammad_bilal/fi_a2_mbilal_loginpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BilalLoginPage extends StatefulWidget {
+  const BilalLoginPage({super.key});
+
+  @override
+  State<BilalLoginPage> createState() => _BilalLoginPageState();
+}
+
+class _BilalLoginPageState extends State<BilalLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
