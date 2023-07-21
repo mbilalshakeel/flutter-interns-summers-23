9d4047daf655374dd4a7a26686d419b7d58d265e
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-07-21 12:22:20 +0500

    Discard the changes from Main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_interns/assesments/assessment_2/fi_a2_muhammad_bilal/fi_a2_mbilal_profile/fi_a2_mbilal_profile.dart';
-//import 'package:flutter_interns/assesments/assessment_2/fi_a2_muhammad_bilal/fi_a2_mbilal_homescreen/fi_a2_mbilal_homescreen.dart';
-//import 'package:flutter_interns/sessions/session_1/s1_home.dart';
+import 'package:flutter_interns/sessions/session_1/s1_home.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -15,10 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Summer Internship \'23',
-     // home: S1Home(),
-      //home:BilalLoginPage(),
-      //home:BilalHomeScreen(),
-      home: BilalProfileScreen(),
+      home: S1Home(),
     );
   }
 }
